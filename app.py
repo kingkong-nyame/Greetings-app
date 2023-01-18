62baa6c9374db2d8a40c838db0762f302397cee2
kingkong-nyame <kingkongnyame2000@gmail.com>
2023-01-18 18:33:31 +0000

    my first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Kingkong'
+@app.route('/')
+@app.route('/home')
+def home():
+    flash("What's your name?")
+    return render_template("home.html")
+
+
+@app.route('/greet', methods=['POST', 'GET'])
+def greet():
+    flash("Hi " + str(  request.form['name_input']) + ", great to see you!!")
+    return render_template("home.html")
