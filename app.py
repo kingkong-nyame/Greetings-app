40bbc7108a64ca980feb79edbc60075282dd9ed8
kingkong-nyame <123007341+kingkong-nyame@users.noreply.github.com>
2023-01-18 20:35:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def home():
 def greet():
     flash("Hi " + str(  request.form['name_input']) + ", great to see you!!")
     return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
+    
